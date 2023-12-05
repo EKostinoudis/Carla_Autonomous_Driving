2446217e28005f8878d934862ca4dd6576a920ba
EKostinoudis <EKostinoudis@protonmail.com>
2023-12-05 23:48:43 +0200

    fixed laucher bugs

--- environment/carla_launcher.py
+++ environment/carla_launcher.py
@@ -1,5 +1,8 @@
 import subprocess
 import time
+import logging
+
+logger = logging.getLogger(__name__)
 
 class CarlaLauncher():
     def __init__(self, port, launch_script, restart_after=-1, sleep=5.):
@@ -10,12 +13,18 @@ class CarlaLauncher():
         restart_after: restart the server after every given value, if it is 
             negative never restart
         '''
-        self.port = port
-        self.launch_script = launch_script
+        self.launch_script = launch_script + f' {port}'
         self.restart_after = restart_after
         self.sleep = sleep
         self.server = None
         self.count_resets = 0
+
+        self.lauch()
+
+        # TODO: maybe try to connect to the server and set it on sync mode, in
+        #       order to avoid this sleep
+        # extra sleep time for the cold start, also we may lauch many servers
+        time.sleep(self.sleep * 20)
 
     def reset(self):
         if self.restart_after >= 0:
@@ -29,8 +38,9 @@ class CarlaLauncher():
     def lauch(self):
         self.kill()
 
+        logger.info(f'Lauching server: {self.launch_script}')
         self.server = subprocess.Popen(
-            [self.launch_script, self.port],
+            self.launch_script,
             shell=True,
         )
         time.sleep(self.sleep)
@@ -38,6 +48,7 @@ class CarlaLauncher():
 
     def kill(self):
         if self.server is not None:
+            logger.info('Killing server')
             self.server.kill()
-        time.sleep(self.sleep)
+            time.sleep(self.sleep)
 
